e7ea43d19704e5e3ae6fa8e91cd16562541122b2
sidhart25 <navneetnishchal1420@gmail.com>
2024-04-21 10:58:58 +0530

    some issues resolved again for vercel

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/about", (req, res) => {
+  res.send("This is my about route..... ");
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
